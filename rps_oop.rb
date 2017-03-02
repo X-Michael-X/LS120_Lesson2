f8c94818921f8fd832cc82fadc8853d6de431c1c
Michael Porter <mdporter83@gmail.com>
2017-03-02 15:30:20 +0000

    to be checked

--- rps_oop.rb
+++ rps_oop.rb
@@ -1,6 +1,7 @@
+require 'pry'
 
 class RPSGame
-  attr_reader :record
+  attr_reader :record, :human_choices
   attr_accessor :human, :computer
 
   def initialize
@@ -9,6 +10,11 @@ class RPSGame
     @human_score = 0
     @computer_score = 0
     @record = []
+    @human_choices = []
+  end
+  
+  def clear_screen
+    system('clear') || system('cls')
   end
 
   def display_welcome_message
@@ -22,55 +28,62 @@ class RPSGame
   def display_choices
     puts "#{human.name} chose : #{human.move}."
     puts "#{computer.name} chose : #{computer.move}."
-    record << ["#{human.name}/#{human.move} ,#{computer.name}/#{computer.move}"]
+    record << [human.name, human.move, computer.name, computer.move]
   end
 
   def display_winner
     if human.move > computer.move
+      human_choices << (human.move).to_s
       human.increase_score
       puts "#{human.name} Won the Round! You have #{human.score} points."
-      if human.score == 3
-        puts "#{human.name} earned 3 points first! You Won the Game!"
-      end
     elsif computer.move > human.move
       computer.increase_score
       puts "#{computer.name} Won the Round! They have #{computer.score} points."
-      if computer.score == 3
-        puts "#{computer.name} earned 3 points first! They Won the Game!"
-      end
     else
       puts "It's a Tie!"
     end
   end
-
+  
   def display_record
-    record.each_with_index do |_round, index|
-      puts "Round #{index + 1}: #{human.name} chose #{human.move}, " \
-           "#{computer.name} chose #{computer.move}"
+    puts "Would you like to see the match record (y/n)?"
+    choice = gets.chomp.downcase
+    return unless choice == 'y'
+    record.each_with_index do |round, index|
+      puts "Round #{index + 1}: #{human.name} chose #{record[index][1]}, " \
+           "#{computer.name} chose #{record[index][3]}."
     end
   end
 
   def play_again
     choice = nil
     loop do
-      puts "Would you like to play again (Yes or No)?"
+      puts "Would you like to play again (y/n)?"
       choice = gets.chomp.downcase
-      break if ['yes', 'no'].include? choice
+      clear_screen
+      break if ['y', 'n'].include? choice
       puts "Invalid Answer."
-    end
+  end
 
-    return if choice == 'no'
+    return if choice == 'n'
     human.choose
-    computer.choose
+    if human.score - computer.score >= 2
+      computer.adapt
+    else
+      computer.choose
+    end
     display_choices
     display_winner
-    return if game_over?
     display_record
+    return if game_over?
     play_again
   end
 
   def game_over?
-    human.score == 3 || computer.score == 3
+    if human.score == 10
+      return "#{human.name} earned 10 points first! You Won the Game!"
+    elsif computer.score == 10
+      return "#{computer.name} earned 10 points first! They Won the Game!"
+    end
   end
 
   def play
@@ -133,6 +146,7 @@ class Player
 end
 
 class Human < Player
+  
   def initialize
     set_name
     self.score = 0
@@ -152,8 +166,9 @@ class Human < Player
   def choose
     choice = nil
     loop do
-      puts "choose rock, paper, scissors, lizard, or Spock"
+      puts "#{self.name}, please choose Rock, Paper, Scissors, Lizard, or Spock"
       choice = gets.chomp.downcase
+      display_record if choice == 'record'
       break if Move::VALUES.include? choice
       puts 'Incorrect input.'
     end
@@ -162,29 +177,35 @@ class Human < Player
 end
 
 class Computer < Player
+  
   def initialize
     set_name
     self.score = 0
   end
 
   def set_name
-    self.name = ['Johnny 5', 'HAL', 'R2D2'].sample
+    self.name = ['Johnny 5', 'HAL', 'R2D2', 'BB-8', 'Deep Blue'].sample
   end
 
   def choose
-    if name == 'Johnny 5'
+    if name == ('Johnny 5' || 'Deep Blue')
       self.move = Move.new(Move::VALUES[0])
-    elsif name == 'HAL'
+    elsif name == ('HAL' || 'BB-8')
       result = (1..10).to_a.sample
       self.move = if (1..7).cover?(result)
                     Move.new(Move::VALUES[2])
                   else
-                    Move.new(Move::VALUES[0])
+                    Move.new(Move::VALUES.sample)
                   end
     else
       self.move = Move.new(Move::VALUES.sample)
     end
   end
+  
+  def adapt
+    self.move = Move.new(Move::VALUES.sample)
+  end  
 end
 
 RPSGame.new.play
+
